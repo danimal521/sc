2a363b31326fd2c0833bf54de9183e86af0e7c9f
Dan Biscup <dabiscup@microsoft.com>
2026-02-20 22:19:33 -0700

    1

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
-st.title("SmartCatAI")
+
+
+
+
+
+left, right = st.columns([4, 1], vertical_alignment="center")
+
+with left:
+    st.title("SmartCatAI")
+
+with right:
+    st.image("assets/b553.png", use_container_width=True)
+
+
+
+
+
+
+
+
 
 fileTrade = st.file_uploader("eTrade Portfolio File", type=["csv"])
