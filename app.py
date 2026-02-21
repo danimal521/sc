e39596c02592976e6304ece9281dc5b4c79af7e1
Dan Biscup <dabiscup@microsoft.com>
2026-02-21 08:20:17 -0700

    2

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+from datetime import datetime, timedelta
+import requests
+API_KEY = st.secrets["POLYGON_API_KEY"]
 
+def get_recent_ex_dividend_dates(ticker):
+    url = 'https://api.polygon.io/v3/reference/dividends'
+    params = {
+        'ticker': ticker,
+        'order': 'desc',  # Most recent first
+        'sort': 'ex_dividend_date',
+        'limit': 24,  # Get last 24 months
+        'apiKey': API_KEY
+    }
 
+    dtPast = datetime.now() - timedelta(days=52)
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    aData = []
+    print(f"Count: {len(data['results'])}")
+    if 'results' in data:
+        for dividend in data['results']:
+            #print(dividend["ex_dividend_date"])
+            if datetime.strptime(dividend['ex_dividend_date'], "%Y-%m-%d") > dtPast:
+                aData.append(dividend['ex_dividend_date'])
+
+        return aData
+    return []
 
 
 
@@ -13,8 +39,8 @@ with right:
     st.image("assets/b553.png", use_container_width=True)
 
 
-db_user = st.secrets["DB_USER"]
-st.write(db_user)
+
+st.write(get_recent_ex_dividend_dates("AAPL"))
 
 
 
