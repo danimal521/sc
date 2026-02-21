3d965ea996127fb3e53ead6ac8642cfd87868832
Dan Biscup <dabiscup@microsoft.com>
2026-02-21 08:56:04 -0700

    2

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
+import csv
+import io
 import streamlit as st
 from datetime import datetime, timedelta
 import requests
 API_KEY = st.secrets["POLYGON_API_KEY"]
+
+class CPortItem:
+    def __init__(self, symbol):
+        self.symbol = symbol       
+
+    def __repr__(self):
+        return f"{self.symbol}"
+    
+def load_port(file):
+    a = []
+    try:
+        text_stream = io.TextIOWrapper(fileTrade, encoding="utf-8")
+        reader = csv.reader(text_stream)
+        
+        for _ in range(11):
+            next(reader)
+
+        for row in reader:
+            try:
+                if row[0] == "CASH":
+                    return a
+                cPI = CPortItem(row[0])
+                a.append(cPI)               
+            except:
+                continue
+    
+    except FileNotFoundError:
+        st.error("File not found. Please check the file path and try again.")   
+
+    return a
 
 def get_recent_ex_dividend_dates(ticker):
     url = 'https://api.polygon.io/v3/reference/dividends'
@@ -28,8 +60,6 @@ def get_recent_ex_dividend_dates(ticker):
         return aData
     return []
 
-
-
 left, right = st.columns([4, 1], vertical_alignment="center")
 
 with left:
@@ -38,15 +68,11 @@ with left:
 with right:
     st.image("assets/b553.png", use_container_width=True)
 
-
-
-st.write(get_recent_ex_dividend_dates("AAPL"))
-
-
-
-
-
-
-
-
 fileTrade = st.file_uploader("eTrade Portfolio File", type=["csv"])
+
+if fileTrade is not None:
+    with st.spinner("Please wait..."):
+        aReport = []
+        aPort = load_port(fileTrade)
+        for item in aPort:
+            st.write(f"Processing {item.symbol}")
